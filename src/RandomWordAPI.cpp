671c40fe4f8ddf8061af2d61100f3d050ee70caa
Yann Osmont <yann.osmont.external-provider@kuhn.com>
2023-10-31 10:10:38 +0100

    .

--- src/RandomWordAPI.cpp
+++ src/RandomWordAPI.cpp
@@ -5,8 +5,12 @@
 
 std::vector<std::string> rwapi::RandomWordAPI::GetWordList(const int &number, const int &length, const std::string &lang)
 {
-    cpr::Response r = cpr::Get(cpr::Url{BASEURL + "?lang=" + lang + "&number=" + std::to_string(number) + "&length=" + std::to_string(length)});
-    std::cout << r.text << std::endl;
+    cpr::Response r = cpr::Get(
+        cpr::Url{BASEURL + "?lang=" + lang + "&number=" + std::to_string(number) + "&length=" + std::to_string(length)},
+        cpr::Authentication{"user", "pass", cpr::AuthMode::BASIC},
+        cpr::Parameters{}
+    );
+    std::cout << r.status_code << " - " << r.text << std::endl;
     nlohmann::json json{r.text};
     std::vector<std::string> ret{};
     for (auto elem : json) {
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 int main(void){
   std::srand(std::time(nullptr));
-#if 0
+#if 1
   rwapi::RandomWordAPI::GetWordList(10, 5);
   exit(0);
 #endif
